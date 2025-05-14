861962052f5c7a8da6be5c5f1bd8fdef2f19d60d
raj <imraaj10262gmail.comclear>
2025-05-14 14:45:54 +0530

    first commit

---
+++ CLASS 1/app.js
@@ -0,0 +1,110 @@
+//Fundamentals of JS
+// arrays and objects
+// function return
+// async js coding
+// foreach map filter find indexof
+
+// FOREACH
+
+// var arr = [1,2,3,4];
+// arr.forEach(function(val){
+//     console.log(val+' hello');
+    
+// })
+
+// MAP
+
+// var arr= [1,2,3,4];
+// var newArr =arr.map(function(val){
+//     // return 13;
+//     return val*3;
+// })
+// console.log(newArr);
+
+
+// FILTER
+
+// var arr= [1,2,3,4];
+// var ans =arr.filter(function(val){
+//     if(val>3){
+//         return true;
+//     }else return false;
+    
+// })
+
+// console.log(ans);
+
+// FIND
+
+// var arr= [1,2,3,4];
+// var ans2 = arr.find(function(val){
+//     if(val===2) return val;
+//     else return false;
+// })
+
+// console.log(ans2);
+
+
+// INDEXOF
+// var arr= [1,2,3,4];
+
+// console.log(
+// arr.indexOf(12)
+
+// );
+
+// OBJECTS
+
+// var obj = {
+//     name:"raj",
+//     age:21
+// }
+// obj['name']
+// obj.name
+
+// obj.age=25;
+
+// Object.freeze(obj)
+
+// obj.age=15;
+
+
+// how to find funciton lenght
+
+// function abc(){
+
+// }
+
+// abc.length
+
+// function abc(a,b,c){
+
+// }
+
+// abc.length
+
+
+// how RETURN works
+
+// function abcd(){
+//     return 12;
+// }
+
+
+// var ans = abcd();
+// console.log(ans);
+
+// await to use krne ke liye async ke bina bhi chl skta h
+
+// async js
+
+// async function abcd() {
+//     var blob = await fetch('https://randomuser.me/api/');
+//     var ans = await blob.json();
+
+//     console.log(ans);
+//     console.log(ans.results[0].name);
+    
+// }
+// abcd();
+
